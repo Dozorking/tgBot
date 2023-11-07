725f34d87e44fbb38b93c5a7181f77743045eb89
Dozorking <150114680+Dozorking@users.noreply.github.com>
2023-11-07 17:06:07 +0700

    Add files via upload

---
+++ Bot.py
@@ -0,0 +1,41 @@
+import telebot
+from special_config import keys, TOKEN
+from extensions import CryptoConverter, ConvertionExeption
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start', 'help'])
+def help(message: telebot.types.Message):
+    text = 'Чтобы начать работу введите команду боту в следующем формате:\n<имя валюты, цену которой вы хотите узнать> \
+<имя валюты в которой надо узнать цену первой валюты> \
+<количество первой валюты>\nУвидеть список всех доступных валют: /values'
+    bot.reply_to(message, text)
+
+@bot.message_handler(commands=['values'])
+def values(message: telebot.types.Message):
+    text = 'Доступные валюты:'
+    for key in keys.keys():
+        text = '\n'.join((text, key,))
+    bot.reply_to(message, text)
+
+@bot.message_handler(content_types=['text', ])
+def convert(message: telebot.types.Message):
+    try:
+        values = message.text.split(' ')
+
+        if len(values) != 3:
+            raise ConvertionExeption('Слишком много параметров.')
+
+        quote, base, amount = values
+        total_base = CryptoConverter.convert(quote, base, amount)
+    except ConvertionExeption as e:
+        bot.reply_to(message, f'Ошибка пользователя\n{e}')
+    except Exception as e:
+        bot.reply_to(message, f'Неудалось обработать команду\n{e}')
+    else:
+        text = F'Цена {amount} {quote} в {base} - {total_base}'
+        bot.send_message(message.chat.id, text)
+
+
+bot.polling()
+
+
---
+++ special_config.py
@@ -0,0 +1,6 @@
+TOKEN = '6317557571:AAGAgVnmZz99W8VLuop9nxKaFeJ0h2axy_Y'
+keys = {
+    'рубль': 'RUB',
+    'евро': 'EUR',
+    'доллар': 'USD'
+}
